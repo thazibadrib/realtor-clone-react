5e9d98a46879123343e4048c008e603521ba464d
unknown <thazibadrib007@gmail.com>
2023-08-12 09:28:14 -0230

    Add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,15 +6,15 @@ import { db } from "../firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { FaShare } from "react-icons/fa";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { FaShare, FaMapMarkerAlt, FaBed, FaBath, FaChair, FaParking } from "react-icons/fa";
 
 export default function Listing() {
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
-  const[shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   const params = useParams();
 
   //Swiper.use(EffectFade, Autoplay, Pagination, Navigation);
@@ -39,10 +39,10 @@ export default function Listing() {
   return (
     <main>
       <Swiper
-        modules={[Navigation, Pagination,Autoplay,EffectFade]}
+        modules={[Navigation, Pagination, Autoplay, EffectFade]}
         slidesPerView={1}
         navigation
-        pagination={{ clickable:true, type: "progressbar" }}
+        pagination={{ clickable: true, type: "progressbar" }}
         effect="fade"
         autoplay={{ delay: 3000 }}
       >
@@ -58,18 +58,75 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center " onClick={()=>{
-        navigator.clipboard.writeText(window.location.href);
-        setShareLinkCopied(true); 
-        setTimeout(()=>{
+      <div
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center "
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
             setShareLinkCopied(false);
-        },2000)
-      }}>
-        <FaShare className="text-xl text-slate-500 "/>
+          }, 2000);
+        }}
+      >
+        <FaShare className="text-xl text-slate-500 " />
       </div>
       {shareLinkCopied && (
-        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2 ">Link Copied</p>
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2 ">
+          Link Copied
+        </p>
       )}
+       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+        <div className=" w-full h-[200px] lg-[400px]">
+          <p className="text-2xl font-bold mb-3 text-blue-900">
+            {listing.name} - ${" "}
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" ? " / month" : ""}
+          </p>
+          <p className="flex items-center mt-6 mb-3 font-semibold">
+            <FaMapMarkerAlt className="text-green-700 mr-1" />
+            {listing.address}
+          </p>
+          <div className="flex justify-start items-center space-x-4 w-[75%]">
+            <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white text-center font-semibold shadow-md">
+              {listing.type === "rent" ? "Rent" : "Sale"}
+            </p>
+            {listing.offer && (
+              <p className="w-full max-w-[200px] bg-green-800 rounded-md p-1 text-white text-center font-semibold shadow-md">
+                ${+listing.regularPrice - +listing.discountedPrice} discount
+              </p>
+            )}
+          </div>
+          <p className="mt-3 mb-3">
+            <span className="font-semibold">Description - </span>
+            {listing.description}
+          </p>
+          <ul className="flex items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+            <li className="flex items-center whitespace-nowrap">
+              <FaBed className="text-lg mr-1" />
+              {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaBath className="text-lg mr-1" />
+              {+listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaParking className="text-lg mr-1" />
+              {listing.parking ? "Parking spot" : "No parking"}
+            </li>
+            <li className="flex items-center whitespace-nowrap">
+              <FaChair className="text-lg mr-1" />
+              {listing.furnished ? "Furnished" : "Not furnished"}
+            </li>
+          </ul>
+        </div>
+        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+      </div>
     </main>
   );
 }
